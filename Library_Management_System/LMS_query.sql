06a08c449134bfee3322bfc4f5727ff53141f271
tushar2704 <tushar.inseec@gmail.com>
2023-06-16 00:27:19 +0530

    update

--- Library_Management_System/LMS_query.sql
+++ Library_Management_System/LMS_query.sql
@@ -4,8 +4,12 @@ Queries
 */
 /* ======================= Query & Solutions ========================*/
 
-/*1)---------------
----------*/
+/*1)List customers who have overdue books*/
+SELECT c.first_name, c.last_name, c.email, c.phone_number, b.title, i.due_date
+FROM customers AS c
+JOIN issued_books AS i ON c.id = i.customer_id
+JOIN books AS b ON i.book_id=b.id
+WHERE i.return_date IS NULL AND i.due_date < CURRENT_DATE;
 
-
+/*2)List the most popular books (most issued)*/
 
