fdfc8786bf674b8a43bd2b3a516232fd837bca04
Tushar Aggarwal <tushar.inseec@gmail.com>
2023-06-21 00:38:14 +0530

    Update Step8.sql

--- Bank_Accounts_Management_System/Step8.sql
+++ Bank_Accounts_Management_System/Step8.sql
@@ -1,29 +1,25 @@
-To implement Step 8, which involves documentation and presentation of your PostgreSQL-based project, consider the following guidelines:
+/*Documentation and presentation of your PostgreSQL-based project:*/
 
-1. Documenting the architecture:
+/*1. Documenting the architecture:
    - Describe the overall architecture of your Bank Accounts Management System, including the database schema, table relationships, and key functionalities.
    - Explain the rationale behind your design choices, such as the selection of specific data types, table structures, and security measures.
-   - Provide an overview of the technologies used, including the programming language, frameworks, and libraries employed in your project.
+   - Provide an overview of the technologies used, including the programming language, frameworks, and libraries employed in your project.*/
 
-2. Documenting the functionalities:
+/*2. Documenting the functionalities:
    - Detail the different features of your system, such as user registration, login, user management, transaction handling, security measures, and reporting capabilities.
    - Describe how each functionality works, including the underlying SQL queries, stored procedures, or functions used to implement them.
-   - Include examples or code snippets to illustrate the usage of different functionalities.
+   - Include examples or code snippets to illustrate the usage of different functionalities.*/
 
-3. Documenting the setup and deployment:
+/*3. Documenting the setup and deployment:
    - Provide instructions on how to set up the Bank Accounts Management System, including installing the necessary software, configuring the database connection, and running any required scripts or migrations.
-   - Explain any specific deployment considerations, such as server requirements, environment variables, and configuration files.
+   - Explain any specific deployment considerations, such as server requirements, environment variables, and configuration files.*/
 
-4. Documenting usage and APIs:
+/*4. Documenting usage and APIs:
    - Describe how users can interact with your system, including the user interface or API endpoints available.
    - Provide documentation for any APIs, specifying the available endpoints, required parameters, and expected responses.
-   - Include examples of API requests and responses to illustrate how to use the system programmatically.
+   - Include examples of API requests and responses to illustrate how to use the system programmatically.*/
 
-5. Presenting the project:
+/*5. Presenting the project:
    - Create a presentation summarizing your Bank Accounts Management System, highlighting its features, design principles, and implementation details.
    - Showcase any unique or innovative aspects of your project, such as advanced security measures, performance optimizations, or user-friendly interfaces.
-   - Demonstrate how the system addresses the initial requirements and how it can be beneficial in a real-world banking scenario.
-
-Ensure that your documentation is comprehensive, clear, and well-organized. Use appropriate formatting, diagrams, and screenshots to enhance readability. Consider using popular documentation formats like Markdown or HTML for ease of sharing and accessibility.
-
-By documenting and presenting your project effectively, you can provide valuable insights to others and ensure that your work is reproducible and maintainable.
+   - Demonstrate how the system addresses the initial requirements and how it can be beneficial in a real-world banking scenario.*/
